ff04ea420092d6e5119e68f6f2276bf565d44b9e
Zeke Dou <zeke@zekedou.live>
2026-04-21 20:39:01 +0800

    fix: remove useless config

--- config/dev.exs
+++ config/dev.exs
@@ -6,10 +6,6 @@ config :logger, :default_formatter, format: "[$level] $message\n"
 # Initialize plugs at runtime for faster development compilation.
 config :combo, :plug_init_mode, :runtime
 
-# Include CEEx debug annotations as HTML comments in rendered markup.
-# Changing it requires `mix clean` and a full recompile.
-config :combo, :template, ceex_debug_annotations: true
-
 # Set a higher stacktrace during development.
 # Avoid configuring it in production as building large stacktraces may be expensive.
 config :my_app, :stacktrace_depth, 20
